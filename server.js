7135a16795718d19eb51d409f89b2c806ce16453
Megumi Miyatani <megumi.miyatani@gmail.com>
2021-04-23 15:43:32 +0900

    public pathを消してみる

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.use(express.static(__dirname));
 app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), () => {
     console.log('Derp is running at localhost: 8080');
-}); 
+});
