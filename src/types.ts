8bcdb9d4d411ba5d3846d54a56ee2562235aa03a
Megumi Miyatani <megumi.miyatani@gmail.com>
2021-05-13 21:06:25 +0900

    add: types.ts

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type Produce = {
+  id: number | undefined
+  name: string
+  description: string | null
+  price: number
+  is_disabled: boolean
+  created_at: string
+  updated_at: string
+}
+
+export type Review = {
+  id: number | undefined
+  item_id: number
+  name: string
+  content: string
+  star: number
+  created_at: string
+  updated_at: string
+}
