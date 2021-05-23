0967de034b77f43dd0cd7a152129f4a7487aa767
Megumi Miyatani <megumi.miyatani@gmail.com>
2021-05-23 17:18:52 +0900

    add: components/global

--- src/components/admin/AdminItemForm.vue
+++ src/components/admin/AdminItemForm.vue
@@ -1,8 +1,4 @@
 <template>
-  <div class="flex">
-    <h1 class="text-xl">商品詳細</h1>
-  </div>
-
   <div class="flex justify-center">
     <div class="my-10 item-img bg-gradient-to-br bg-yellow-200 w-3/12 h-3/12"></div>
   </div>
@@ -10,88 +6,83 @@
   <label class="block my-12 sm:flex items-center">
     <div class="text-gray-700 w-1/6">商品名</div>
 
-    <div v-if="!product">Waiting..</div>
-
-    <input
-      v-else
-      type="text"
-      class="p-3 mt-3 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-      v-model="product.name"
-    />
+    <div v-if="!anyLoading" class="w-full">
+      <input-form v-model="product.name" />
+    </div>
+    <div v-else>Loading...</div>
   </label>
 
   <label class="block my-12 sm:flex items-center">
     <div class="text-gray-700 w-1/6">商品説明</div>
-
-    <div v-if="!product">Waiting..</div>
-
-    <textarea
-      v-else
-      class="p-3 mt-3 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-      rows="3"
-      v-model="product.description"
-    ></textarea>
+    <div v-if="!anyLoading" class="w-full">
+      <textarea-form v-model="product.description" />
+    </div>
+    <div v-else>Loading...</div>
   </label>
 
   <label class="block my-12 sm:flex items-center">
     <div class="text-gray-700 w-1/6">価格</div>
 
-    <div v-if="!product">Waiting..</div>
+    <div v-if="!anyLoading" class="w-full">
+      <input-form v-model="product.price"></input-form>
+    </div>
 
-    <input
-      v-else
-      type="text"
-      class="p-3 mt-3 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-      v-model="product.price"
-    />
+    <div v-else>Loading...</div>
   </label>
 
-  <button @click="onSaveClick" class="btn btn-primary" :disabled="!product">登録</button>
+  <!--  <button @click="onSaveClick" class="btn btn-primary" :disabled="!product || anyLoading">-->
+  <!--    登録-->
+  <!--  </button>-->
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, WritableComputedRef, onMounted } from "vue"
+import { defineComponent, computed, WritableComputedRef } from "vue"
 import { Product } from "@/types"
-import { productState, getAndSetProduct } from "@/composables/useProducts"
+import InputForm from "@/components/global/InputForm.vue"
+import TextareaForm from "@/components/global/TextareaForm.vue"
 
 export default defineComponent({
   name: "adminItemForm",
 
-  components: {},
+  components: {
+    TextareaForm,
+    InputForm,
+  },
 
   props: {
-    id: {
-      required: false,
-    },
     modelValue: {
-      type: Object as () => Product,
+      type: Object as () => Product | null,
       required: true,
+    },
+    loading: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
   },
 
   emits: ["update:modelValue", "save"],
 
   setup: function (props, { emit }) {
-    onMounted(() => {
-      // getAndSetProduct(props.id)
-    })
+    // const onSaveClick = (): void => {
+    //   emit("save")
+    // }
 
-    const onSaveClick = (): void => {
-      emit("save")
-    }
-
-    const product: WritableComputedRef<Product> = computed({
-      get(): Product {
+    const product: WritableComputedRef<Product | null> = computed({
+      get(): Product | null {
         return props.modelValue
       },
       set(): void {
-        emit("update:modelValue", productState.product)
+        emit("update:modelValue", product)
       },
     })
 
+    const anyLoading: WritableComputedRef<boolean> = computed(() => props.loading || !product.value)
+
     return {
       product,
-      onSaveClick,
+      anyLoading,
+      // onSaveClick,
     }
   },
 })
---
+++ src/components/global/InputForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <input type="text" v-model="value" />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, WritableComputedRef } from "vue"
+
+export default defineComponent({
+  name: "InputForm",
+  components: {},
+  props: {
+    modelValue: {
+      type: String,
+      required: false,
+    },
+  },
+  // props: ["modelValue"],
+  emits: ["update:modelValue"],
+
+  setup: function (props, { emit }) {
+    const value: WritableComputedRef<any> = computed({
+      get(): string | undefined {
+        return props.modelValue
+      },
+      set(value): void {
+        emit("update:modelValue", value)
+      },
+    })
+
+    return {
+      value,
+    }
+  },
+})
+</script>
+<style scoped>
+input {
+  @apply p-3 mt-3 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50;
+}
+</style>
