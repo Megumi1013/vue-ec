378f390fec13f583784c8fc41fa60ba4e4279ee6
Megumi Miyatani <megumi.miyatani@gmail.com>
2021-05-24 23:02:05 +0900

    Add dist folder to .gitignore
    Fix server.js script to run from correct folder

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
-var app = express();
-var history = require('connect-history-api-fallback');
+var path = require('path');
+var serveStatic = require('serve-static');
 
-app.use(history());
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
 
-app.use(express.static(__dirname));
+var port = process.env.PORT || 5000;
 
-app.set('port', (process.env.PORT || 8080));
-app.listen(app.get('port'), () => {
-    console.log('Derp is running at localhost: 8080');
-});
+app.listen(port);
+console.log('server started '+ port);
